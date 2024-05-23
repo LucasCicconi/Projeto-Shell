93ec31b5dc2249198e80090967b982aa302572e2
Rafael Umetsu <rafael.umetsu@gmail.com>
2024-05-23 13:52:21 -0300

    Update ls to resolve paths Update ls to accept multiples paths (like unix ls)

--- ls/ls.c
+++ ls/ls.c
@@ -1,20 +1,32 @@
-//ls.c
+// ls.c
+#include <dirent.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <pwd.h>
-#include <grp.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
-void print_file_info(const char *name) {
+void print_file_info(const char *path, const char *name) {
     struct stat file_stat;
-    if (stat(name, &file_stat) < 0) {
-        perror("ls");
+
+    char *whole_path = (char *)malloc(sizeof(path) + 1 + sizeof(name) + 1);
+
+    strcpy(whole_path, path);
+    strcat(whole_path, "/");
+    strcat(whole_path, name);
+    strcat(whole_path, "\0");
+
+    if (stat(whole_path, &file_stat) < 0) {
+        fprintf(stderr, "ls -l: %s: %s\n", name, strerror(errno));
+        free(whole_path);
         return;
     }
+    free(whole_path);
 
     printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
     printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
@@ -48,37 +60,46 @@ int main(int argc, char *argv[]) {
 
     while ((opt = getopt(argc, argv, "la")) != -1) {
         switch (opt) {
-        case 'l':
-            show_long = 1;
-            break;
-        case 'a':
-            show_all = 1;
-            break;
-        default:
-            fprintf(stderr, "Usage: ls [-l] [-a]\n");
-            exit(EXIT_FAILURE);
+            case 'l':
+                show_long = 1;
+                break;
+            case 'a':
+                show_all = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: ls [-l] [-a]\n");
+                exit(EXIT_FAILURE);
         }
     }
 
     DIR *dir;
     struct dirent *entry;
 
-    if ((dir = opendir(".")) == NULL) {
-        perror("ls");
-        exit(EXIT_FAILURE);
-    }
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "-", 1) == 0)
+            continue;
 
-    while ((entry = readdir(dir)) != NULL) {
-        if (!show_all && entry->d_name[0] == '.') {
+        printf("%s:\n", argv[i]);
+
+        if ((dir = opendir(argv[i])) == NULL) {
+            fprintf(stderr, "ls: %s: %s\n", argv[i], strerror(errno));
             continue;
         }
-        if (show_long) {
-            print_file_info(entry->d_name);
-        } else {
-            printf("%s\n", entry->d_name);
+
+        while ((entry = readdir(dir)) != NULL) {
+            if (!show_all && entry->d_name[0] == '.') {
+                continue;
+            }
+            if (show_long) {
+                print_file_info(argv[i], entry->d_name);
+            } else {
+                printf("%s\n", entry->d_name);
+            }
         }
+        printf("\n");
+
+        closedir(dir);
     }
 
-    closedir(dir);
     return 0;
 }
